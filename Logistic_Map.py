4d5890cb951fb0bcca656c44e6ec2646a662cf7d
Scott Pirrie <scott.pirrie@googlemail.com>
2019-10-04 14:01:35 +0100

    Orbit plot function created

--- Logistic_Map.py
+++ Logistic_Map.py
@@ -12,6 +12,8 @@ Created on Fri Oct  4 12:18:20 2019
 # Logistic Map definition - alpha is the single parameter
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def logistic_map(x, alpha):
     l = alpha*x*(1-x)
@@ -36,4 +38,43 @@ def fun_iteration(fun, ntimes, **kwargs):
         next_one = fun(next_one, alpha = alpha)
         print(next_one)
     
-fun_iteration(logistic_map, 200, alpha = 3.4)
+#fun_iteration(logistic_map, 200, alpha = 1.5)
+
+
+def plot_orbit(start = 0.1, alpha = 1):
+    
+    x = np.arange(0,1,0.001)
+    y= logistic_map(x, alpha = alpha)
+    
+    orb_x = []
+    orb_y = []
+    
+    x_i = start
+    
+    for n in range(1000):
+        
+        if n % 2 == 0:
+            orb_x.append(x_i)
+            orb_y.append(x_i)
+            y_i = logistic_map(x= x_i, alpha = alpha)
+            
+        else:
+            orb_x.append(x_i)
+            orb_y.append(y_i)
+            x_i = y_i
+      
+    plt.close()
+    plt.plot(x,x)
+    plt.plot(x,y)
+    plt.plot(orb_x, orb_y)
+    plt.title("Logistic Map for alpha = {}".format(alpha))
+    plt.show()
+    
+
+plot_orbit(alpha = 2.3)    
+    
+    
+    
+
+
+
